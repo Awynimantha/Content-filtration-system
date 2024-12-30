5df4ebf52210c3c1784c13f36aa5ecf1fc158aad
Awynimantha <awynimantha@gmail.com>
2024-12-30 15:03:22 +0530

    eureka initizlized

---
+++ service_discovery/eureka-server/src/test/java/com/service_discovery/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service_discovery.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
